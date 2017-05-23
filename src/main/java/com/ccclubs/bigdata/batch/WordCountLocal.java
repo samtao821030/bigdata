b0c9be59a43440eb5aec185817cc37013b2e2642
taosm <samtao821030@gmail.com>
2017-05-24 00:31:46 +0800

    Initial commit

---
+++ src/main/java/com/ccclubs/bigdata/batch/WordCountLocal.java
@@ -0,0 +1,60 @@
+package com.ccclubs.bigdata.batch;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.FlatMapFunction;
+import org.apache.spark.api.java.function.Function2;
+import org.apache.spark.api.java.function.PairFunction;
+import org.apache.spark.api.java.function.VoidFunction;
+import scala.Tuple2;
+
+import java.util.Arrays;
+import java.util.Iterator;
+
+/**
+ * Created by Administrator on 2017/5/23 0023.
+ */
+public class WordCountLocal {
+    public static void main(String[] args) {
+        SparkConf conf = new SparkConf()
+                .setAppName("wordcount")
+//                .set("spark.driver.memory","500m")
+                .setMaster("local");
+
+        JavaSparkContext sc = new JavaSparkContext(conf);
+
+        JavaRDD<String> lines = sc.textFile(WordCountLocal.class.getResource("/spark.txt").getPath());
+
+        JavaRDD<String> words = lines.flatMap(new FlatMapFunction<String, String>() {
+            public Iterator<String> call(String line) throws Exception {
+                return Arrays.asList(line.split(" ")).iterator();
+            }
+        });
+
+        JavaPairRDD<String,Integer> pairs = words.mapToPair(new PairFunction<String, String, Integer>() {
+            @Override
+            public Tuple2<String, Integer> call(String word) throws Exception {
+                return new Tuple2<String,Integer>(word,1);
+            }
+        });
+
+        JavaPairRDD<String,Integer> wordcounts = pairs.reduceByKey(new Function2<Integer, Integer, Integer>() {
+            @Override
+            public Integer call(Integer v1, Integer v2) throws Exception {
+                return v1+v2;
+            }
+        });
+
+        wordcounts.foreach(new VoidFunction<Tuple2<String, Integer>>() {
+            @Override
+            public void call(Tuple2<String, Integer> wordcount) throws Exception {
+                System.out.println(wordcount._1+" appeared "+wordcount._2);
+            }
+        });
+
+        sc.close();
+
+    }
+}
