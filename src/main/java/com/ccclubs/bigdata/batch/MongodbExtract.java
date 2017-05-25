6e6e3b3cd8f711c1996eb8da58242e1628c83dbb
samtao821030 <samtao821030@gmail.com>
2017-05-25 21:50:00 +0800

    修改了相关pom

---
+++ src/main/java/com/ccclubs/bigdata/batch/MongodbExtract.java
@@ -0,0 +1,18 @@
+package com.ccclubs.bigdata.batch;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.sql.SQLContext;
+import org.apache.spark.sql.SparkSession;
+import scala.tools.nsc.util.ClassPath;
+
+/**
+ * Created by Administrator on 2017/5/25 0025.
+ */
+public class MongodbExtract {
+    public static void main(String[] args) {
+        SparkSession sparkSession = null;
+
+
+    }
+}
